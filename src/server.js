a93b2f9bef69b999c2d787957e26500f1c58eba5
ansango <anibalsantosgo@gmail.com>
2021-07-11 16:38:35 +0200

    prod env

--- src/server.js
+++ src/server.js
@@ -1,19 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
+import dotenv from "dotenv";
 import { routes } from "./routes";
 import { db } from "./db";
 
 const app = express();
+app.use(morgan("dev"));
+dotenv.config();
+
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8080;
+const DB = process.env.MONGO_URI_PROD;
 
 routes.forEach((route) => {
   app[route.method](route.path, route.handler);
 });
 
 const start = async () => {
-  await db.connect("mongodb://localhost:27017");
-  app.listen(8080, () => {
-    console.log("Server is listening on port 8080");
+  await db.connect(DB);
+  app.listen(PORT, () => {
+    console.info(`Meal Tracker is running on: ${PORT}`);
   });
 };
 
